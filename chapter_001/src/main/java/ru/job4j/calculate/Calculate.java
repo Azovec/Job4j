5cd25b81ff37be58250ac9410e1767c7580a3d34
Azovec <romabogdan@mail.ru>
2019-03-17 19:44:26 +0300

    1.3. JavaDoc. Code convention[#90633]

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,22 @@
+package ru.job4j.calculate
+
+
+/**
+* Конструктор, вывод строки в консоль
+* @param arg - arg.
+*/
+
+
+public class Calculate
+{
+	
+	public static void main(String[] args) 	{
+		System.out.println("Hello World");
+	}
+
+
+
+
+
+
+
