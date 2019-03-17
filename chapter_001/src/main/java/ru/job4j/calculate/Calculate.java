7225c7ce5fa39ae6677128026f21e67b0b91f251
Azovec <romabogdan@mail.ru>
2019-03-17 20:44:35 +0300

    1.3.1 Maven Plugins[#90630]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,18 +1,23 @@
-package ru.job4j.calculate
+package ru.job4j.calculate;
+
+ /** 
+ * Calculate.
+ *
+ * @author Roman Bogdan (romabogdan.ru).
+ */
 
 
-/**
-* Конструктор, вывод строки в консоль
-* @param arg - arg.
-*/
-
-
-public class Calculate
-{
+public class Calculate {
+	
+ /**
+ * Конструктор, вывод строки в консоль.
+ * @param args - args.
+ */
 	
 	public static void main(String[] args) 	{
 		System.out.println("Hello World");
 	}
+}
 
 
 
