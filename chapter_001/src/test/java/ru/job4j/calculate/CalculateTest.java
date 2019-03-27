91c0244d2843fbd8a5fe8b4eb5c8be6269b1f346
Azovec <romabogdan@mail.ru>
2019-03-27 21:26:39 +0300

    1.5. Checkstyle[#90635]

--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -1,6 +1,33 @@
 ﻿package ru.job4j.calculate
 
+import org.junit.Test;
+import static org.hamcreast.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ *Test.
+ *
+ *@author Roman Bogdan (romabogdan@mail.ru).
+ *@version $Id$.
+ *@since 0.1.
+ */
+ 
+ 
+
 
 public class CalculateTest{
+	/**
+	* Test echo.
+	*/
+	
+@Test
+   public void whenTakeNameThenThreeEchoPlusName() {
+	   String input = "Roman Bogdan";
+	   String expect = "Echo, echo, echo : Roman Bogdan";
+	   Calculate calc = new Calculate();
+	   String result = calc.echo(imput);
+	   assertThat(result, is(expect));
+	   
+   }
 
 }
